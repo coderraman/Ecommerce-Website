d7eb9dde4999cd0515800a40644820e8996d63ea
coderraman <129317923+coderraman@users.noreply.github.com>
2024-05-14 15:44:53 +0530

    Add files via upload

---
+++ Flipcart_Clone/index.js
@@ -0,0 +1,10 @@
+
+function ToggleInput() {
+    // let input = document.querySelector(".input")
+  let Searchitem = document.querySelector(".Search-item");
+  if (Searchitem.style.display === "none") {
+    Searchitem.style.display = "block";
+  } else {
+    Searchitem.style.display = "none";
+  }
+}
